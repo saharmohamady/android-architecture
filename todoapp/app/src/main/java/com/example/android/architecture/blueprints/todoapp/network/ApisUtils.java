c2e3fe6f067f1b3c06cc0da10d5dcbb53e6b2e5a
sahar almohamady <sahar.almohamady@gmail.com>
2017-06-20 11:39:39 +0200

    remove url constant from code file to gradle

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/network/ApisUtils.java
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/network/ApisUtils.java
@@ -1,13 +1,13 @@
 package com.example.android.architecture.blueprints.todoapp.network;
 
+import com.example.android.architecture.blueprints.todoapp.BuildConfig;
+
 /**
  * Created by Sahar Almohamady on 6/18/2017.
  */
 
 public class ApisUtils {
-    private static String BASE_URL = "https://jsonplaceholder.typicode.com/";
-
     public static GetFaqsApi getFaqsAPIInstance() {
-        return RetrofitNetworkManager.getClient(BASE_URL).create(GetFaqsApi.class);
+        return RetrofitNetworkManager.getClient(BuildConfig.BASE_URL).create(GetFaqsApi.class);
     }
 }
